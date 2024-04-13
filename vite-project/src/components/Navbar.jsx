c54831abe0f48cdcf40526e157d7b9ac0b1ad948
3thernity <3thernitydev@gmail.com>
2024-04-14 00:04:38 +0400

    *responsive de la navbar ok

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/styles/navbar.css"; // Import du fichier CSS pour la barre de navigation
 
 function Navbar() {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
+
     return (
         <nav>
             <div className="logo">
                 <img src="src\assets\images\logo.png" alt="logo" />
                 <h3 className="LogoName">L'éclipse</h3>
             </div>
-            <ul>
+            <ul className={showMenu ? "nav-links active" : "nav-links"}>
                 <li>
                     <a href="/">Accueil</a>
                 </li>
@@ -22,6 +28,11 @@ function Navbar() {
                     <a href="#contact">Contact</a>
                 </li>
             </ul>
+            <div className="burger" onClick={toggleMenu}>
+                <div className={showMenu ? "line1 active" : "line1"}></div>
+                <div className={showMenu ? "line2 active" : "line2"}></div>
+                <div className={showMenu ? "line3 active" : "line3"}></div>
+            </div>
         </nav>
     );
 }
